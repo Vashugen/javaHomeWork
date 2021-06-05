23f4aa57c582c6d495c0482bd73c0ed1f2599ee6
Vashugen <vashu_gen@mail.ru>
2021-06-06 00:57:53 +0300

    realized with class GasStation

--- 6/com/orion/lesson6/taskB/TaskB.java
+++ 6/com/orion/lesson6/taskB/TaskB.java
@@ -2,44 +2,33 @@ package com.orion.lesson6.taskB;
 
 import com.orion.lesson6.tuple.Triple;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TaskB {
 
-    public enum GasolineType {
-        DT("ДТ"),
-        AI92("АИ-92"),
-        AI95("АИ-95"),
-        AI98("АИ-98");
-
-        private final String name;
-
-        GasolineType(String name) {
-            this.name = name;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-
-    }
-
-    List<Triple<String, GasolineType, Double>> gasStationList = List.of(
-            new Triple<String, GasolineType, Double>("Горького, 18", GasolineType.AI92, 95.50),
-            new Triple<String, GasolineType, Double>("Гагарина, 222", GasolineType.DT, 94.50),
-            new Triple<String, GasolineType, Double>("Рокоссовского, 10", GasolineType.AI95, 96.00),
-            new Triple<String, GasolineType, Double>("Минина, 1", GasolineType.AI92, 94.00)
+    List <GasStation> gasStations = List.of(
+            new GasStation ("Горького, 18", GasStation.GasolineType.AI92, 95.50),
+            new GasStation ("Гагарина, 222", GasStation.GasolineType.DT, 94.50),
+            new GasStation ("Рокоссовского, 10", GasStation.GasolineType.AI95, 96.00),
+            new GasStation ("Минина, 1", GasStation.GasolineType.AI92, 94.00)
     );
+
+    List<Triple<String, GasStation.GasolineType, Double>> gasStationList = new ArrayList<Triple<String,GasStation.GasolineType, Double>>(gasStations.forEach(e -> ););
+
+
 
     List<Triple<String,Integer,Double>> mobileAppList = List.of(
             new Triple<String,Integer,Double>("ShmugenGames", 5000, 5.0),
             new Triple<String,Integer,Double>("VashuNote", 25149, 4.69),
             new Triple<String,Integer,Double>("Another one app", 785, 3.25),
-            new Triple<String,Integer,Double>("ShmugenGames", 127000, 4.0),
+            new Triple<String,Integer,Double>("ShmugenGames", 127000, 4.0)
     );
 
-    public static void findBestPrice(GasolineType type){
-        //sout адрес с наименьшей ценой на переданный тип бензина
+    public static void findBestPrice(GasStation.GasolineType type){
+
+        this.gasStationList.forEach();
+        //System.out.println(this.gasStationList);
+
     }
 }
